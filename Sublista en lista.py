8d12816152058f9f1f44c96bbbfbe85aa1e350f3
Teban <105476894+Tebann@users.noreply.github.com>
2023-03-02 10:26:01 -0500

    Delete Sublista en lista.py

--- Sublista en lista.py
+++
@@ -1,8 +0,0 @@
-lista = [4.555, "Bienvenidos a la UTP!", [222, "Programacion III"]]
-i = -1
-for elemento in lista:
-    i+=1
-    if type(elemento) == list:
-        print("Lista Detectada!: ",elemento, "en el elemento ", i)
-    else:
-        print ("Lista no detectada en el elemento", i)
