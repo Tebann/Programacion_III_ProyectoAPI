cd20ac2f352e0c9b83914b40a4d2e8d3cbc60ed5
Teban <105476894+Tebann@users.noreply.github.com>
2023-03-02 10:26:24 -0500

    Delete Tabla conversiones.py

--- Tabla conversiones.py
+++
@@ -1,7 +0,0 @@
-print("Tabla de conversión de Celsius a Fahrenheit")
-
-print("Celsius    Fahrenheit")
-
-for celsius in range(0, 101, 10): #Utilizo range() para cumplir que las temperaturas sean multiplos de 10  
-    fahrenheit = (celsius * 9/5) + 32
-    print(celsius,"------->",fahrenheit)
