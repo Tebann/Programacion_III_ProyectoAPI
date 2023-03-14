a6e43b165a30c14eba55756b477fcb26286ed863
Teban <105476894+Tebann@users.noreply.github.com>
2023-03-14 15:27:48 -0500

    Update main.py

--- ProyectoAPI/main.py
+++ ProyectoAPI/main.py
@@ -7,9 +7,7 @@ def main():
 #Funcion principal que ejecuta nuestro programa
     departamento = UI.obtener_departamento()
     limite_registros = int(UI.obtener_limite_registros())
-    
     resultados = API.obtener_datos(departamento, limite_registros)
-    
     UI.mostrar_resultados(resultados)
 
 if __name__ == '__main__':
