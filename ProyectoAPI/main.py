3389edf857ee833614a76413e3f4a41bd81c0fb8
Teban <105476894+Tebann@users.noreply.github.com>
2023-03-16 10:21:19 -0500

    Update main.py

--- ProyectoAPI/main.py
+++ ProyectoAPI/main.py
@@ -1,15 +1,17 @@
 #main.py
 
-#importacion de los modulos correspondientes para el correcto funcionamiento
+#Importacion de los modulos correspondientes para el correcto funcionamiento
 import API
 import UI
 
-def main():
 #Funcion principal que ejecuta nuestro programa
-    departamento = UI.obtener_departamento()
-    limite_registros = int(UI.obtener_limite_registros())
-    resultados = API.obtener_datos(departamento, limite_registros)
-    UI.mostrar_resultados(resultados)
+def main():
+    departamento = UI.obtener_departamento()                #Llamado a la funcion para obtener el departamento
+    UI.comprobar_departamento(departamento.upper())         # Llamado a la funcion para la comprobacion de la existencia del departamento
+    limite_registros = int(UI.obtener_limite_registros())   #Lamado a la funcion para obtener el numero de registro
+    UI.comprobar_limite(limite_registros)                   # Llamado a la funcion para comprobar que el limite sea menor a 1000 
+    resultados = API.obtener_datos(departamento, limite_registros) #Llamado a la funcion para operar los datos ingresados por el usuario
+    UI.mostrar_resultados(resultados)                       #Llamado a la funcion para mostrar el resultado al usuario
 
 #Verificacion main, para ejecutar como el programa principal
 if __name__ == '__main__':
