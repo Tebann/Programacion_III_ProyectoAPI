97311dbe4486578e3f52767c1fe2c17ca680a4b2
Teban <105476894+Tebann@users.noreply.github.com>
2023-03-16 10:19:47 -0500

    Update UI.py

--- ProyectoAPI/UI.py
+++ ProyectoAPI/UI.py
@@ -1,14 +1,33 @@
 #UI.py
 
-def obtener_departamento():
+import API
+
 #Solicitamos el nombre del departamento al usuario
+def obtener_departamento():
     return input("Ingrese el departamento deseado: ")
 
-def obtener_limite_registros():
 #Solicitamos el límite de registros al usuario
-    print("RECOMENDACION: Ingrese un numero de registro pequeño")
+def obtener_limite_registros():
+    print("RECOMENDACION: Ingrese un numero menor a 1000")
     return input("Ingrese el numero de registro deseado: ")
 
-def mostrar_resultados(resultados):
+#Comprobar que el numero de registro sea menor a 1000
+def comprobar_limite(limite):
+    if limite > 1000:
+        input("Numero de registro no valido.")
+        exit()
+    else:
+        return True
+
+#Comprobacion de que el departamento ingresado sea correcto
+lista_dep = API.crear_lista()
+def comprobar_departamento(departamento):
+    if (departamento not in lista_dep):
+        print("Departamento no encontrado...")
+        exit()
+    else:
+        return True
+
 #Mostramos los resultados al usuario
+def mostrar_resultados(resultados):
     print(resultados)
